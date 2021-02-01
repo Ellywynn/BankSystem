d5103fa5084c00caaae4503516c9162590edd3a3
Ilya <57060144+Ellywynn@users.noreply.github.com>
2021-02-01 17:17:00 +0300

    work with files

--- src/Bank.cs
+++ src/Bank.cs
@@ -14,8 +14,8 @@ namespace BankSystem
 			Credit
 		}
 
-		static int numberOfAccounts;
-		decimal balance;
+		static int numberOfAccounts = 0;
+		decimal balance = 0;
 
 		List<BankAccount> accounts = new List<BankAccount>();
 
@@ -25,11 +25,6 @@ namespace BankSystem
 
 		public string HistoryFileName { get; }
 
-		public Bank()
-		{
-			InitBankFromFile();
-		}
-
 		public Bank(string name, decimal initialBalance)
 		{
 			Name = name;
@@ -38,6 +33,21 @@ namespace BankSystem
 			InitBankFromFile();
 		}
 
+		// destructor for saving bank info
+		~Bank()
+		{
+			string dir = $@"{Directory.GetCurrentDirectory()}/banks";
+			string filePath = dir + @$"/bank_{Name}.txt";
+
+			// write baml characteristics into the file
+			string str =
+				"Number of accounts: " + 0 + '\n' +
+				"Balance: " + Balance + '\n';
+
+			File.WriteAllText(filePath, str);
+		}
+
+		// creates bank account
 		public BankAccount CreateAccount(string name, string surname)
 		{
 			BankAccount account = new BankAccount(name, surname, Name);
@@ -47,45 +57,73 @@ namespace BankSystem
 			return account;
 		}
 
+		// load bank info from file
 		private void InitBankFromFile()
 		{
 			// check to null strings
-
 			string dir = $@"{Directory.GetCurrentDirectory()}/banks";
 			string filePath = dir + @$"/bank_{Name}.txt";
+
+			// if dir for bank info doesn't exist, create it
 			if (!Directory.Exists(dir))
 			{
 				Directory.CreateDirectory(dir);
 			}
+
+			// if file info doesn't exist, create it and save
 			if (!File.Exists(filePath))
 			{
 				File.Create(filePath);
 
-				// initial characteristics(balance/name)
-
-				File.WriteAllText(filePath, "Number of accounts: " + 0 + "\n");
+				SaveBankInfo(filePath);
 				return;
 			}
 
-			// load accounts from file
+			// if file does exists(bank already created), load info
+			LoadBankInfo(filePath);
 		}
 
 		private void SaveNewAccount(BankAccount account)
 		{
 			string filePath = $@"{Directory.GetCurrentDirectory()}/account.txt";
 
+			// if file does not exist, create it and write tablet columns
 			if (!File.Exists(filePath))
 			{
 				File.Create(filePath);
 				File.WriteAllText(filePath, "Account number:\tPerson:\t\tBalance:\tBank:\n");
 			}
 
-			string str = $"{String.Format("{0:000000}",account.Number)}\t" +
+			// write account info(id/name surname/balance/bank name)
+			string str =
+				$"{String.Format("{0:000000}",account.Number)}\t" +
 				$"{account.Owner.Name} {account.Owner.Surname}\t" +
 				$"{String.Format("{0:000000000000}", account.Balance)}\t" +
 				$"{Name}\n";
 
 			File.WriteAllText(filePath, str);
+		}
+
+		private void LoadBankInfo(string filePath)
+		{
+			string[] lines = File.ReadAllLines(filePath);
+
+			numberOfAccounts = int.Parse(lines[0]);
+			balance = decimal.Parse(lines[1]);
+		}
+
+		private void SaveBankInfo(string filePath)
+		{
+			string[] str = { 
+				$"Number of accounts: {numberOfAccounts}",
+				$"Balance: {balance}"
+			};
+			File.WriteAllLines(filePath, str);
+		}
+
+		private void SaveBankAccounts()
+		{
+			string path = $@"";
 		}
 	}
 }
